3413f69d72da216de713f2ac9220c0b59725d9a8
LCEMP <132982371+Ebonut@users.noreply.github.com>
2025-05-14 15:42:35 +0300

    Update taxi.lua
    Made everything more user-friendly and removed my minecraft nickname from it

--- taxi.lua
+++ taxi.lua
@@ -1,24 +1,27 @@
-write("Input coordinates: \n x \n y \n z \n")
-x = read()
-y = read()
-z = read()
+a=...
+SERVERID=10 -- Change it to your own
 print("Getting coords...")
 x1,y1,z1 = gps.locate()
-a = {x1,y1,z1}
 print(a)
+function waitForConfirmation()
+read()
+end
+function timeout()
+sleep(20)
+error("Didn't get your confirmation till timeout, run again to retry!")
+end
 if a ~= nil then
 rednet.open("back")
 p = tostring(math.random(-65535,65535))
-s = rednet.send(10,{a,{x,y,z},p}, "taxi")
+s = rednet.send(SERVERID,{a,{x,y,z},p}, "taxi")
 print("Request sent, waiting for answer...")
 s = rednet.receive(p,10)
-sleep(1)
 if s ~= nil then
 print("Success! A drone will soon pick you up. Press <enter> to confirm arrival")
-read()
-rednet.send(10,0,p)
+parralel.waitForAny(waitForConfirmation,timeout)
+rednet.send(SERVERID,0,p)
 else
-printError("Didn't get a server reply, an error occured. Contact drakonmark12345 to help out.")
+printError("Didn't get a server reply, an error occured. Contact the owner to help out.")
 end
 else
 printError("Couldn't get your coordinates. Try contacting your GPS provider or try again later.")
