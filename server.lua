90420a6435c3281729086bb866bcdcc9ac4399c5
LCEMP <132982371+Ebonut@users.noreply.github.com>
2025-05-14 15:37:29 +0300

    Update debugging and rename startup.lua to server.lua
    Reworked debug and name

--- server.lua
+++ server.lua
@@ -1,8 +1,9 @@
-d = peripheral.wrap("right")
-rednet.open("top")
-repeat
-print("Cycle")
+d = peripheral.find("pneumaticcraft:drone_interface")
+rednet.open(peripheral.getName(peripheral.find("modem")))
+while true do
+print("Waiting for orders...")
 id, a = rednet.receive("taxi")
+print("Order recieved, sending confirmation!")
 rednet.send(id,0,a[3])
 pos1 = a[1]
 pos1R = pos1
@@ -13,33 +14,30 @@ end
 for i,v in ipairs(pos1) do
 m = v-5
 pos1[i] = m
-print(m)
 end
-print("")
 for i,v in ipairs(pos1R) do
 m = v+5
-print(m)
 pos1R[i] = m
 end
+print("Sending the drone to user's coordinates...")
 d.clearArea()
 d.addArea(pos1[1],pos1[2],pos1[3],pos1R[1],pos1R[2],pos1R[3],"filled")
-print(textutils.tabulate(pos1))
-print(textutils.tabulate(pos1R))
 d.setAction("teleport")
 repeat
 sleep(0.01) until d.isActionDone()
+print("Picking the user up and waiting for confirmation...")
 d.abortAction()
 d.setAction("entity_import")
-rednet.receive(a[3],120)
+rednet.receive(a[3],20)
+print("Confirmation received, getting the user to their destination...")
 d.clearArea()
 d.abortAction()
 d.addArea(pos2[1],pos2[2],pos2[3])
-print("Second position:")
-print(textutils.tabulate(pos2))
 d.setAction("entity_export")
 sleep(10)
 d.exitPiece()
-until false==true
+print("Done!")
+end
 
 
 
